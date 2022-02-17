74f37121757c8473d78dddbb01434575f55f41ec
MultiTea <lucasbolea@live.fr>
2022-02-17 09:29:48 +0100

    JS script optimization

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,21 @@
-fetch("http://127.0.0.1:5500/api/products")
-  .then((response) => response.json())
-  .then((data) => {
-
-    let product = document.getElementById("items");
-
-    // Products creation : loop to add each product to DOM
-    for (i = 0; i < data.length; i++) {
-      product.innerHTML += `
+// Affichage produits
+let showProduct = (data) => {
+  let items = document.getElementById('items')
+  for (let i in data) {
+    items.innerHTML += `
     <a href="./product.html?id=${data[i]._id}">
       <article>
         <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
         <h3 class="productName">${data[i].name}</h3>
         <p class="productDescription">${data[i].description}</p>
       </article>
-    </a>`;
-    }
-  });
+    </a>`
+  }
+}
+
+// Connexion à l'API
+fetch('http://localhost:3000/api/products')
+  .then((response) => response.json())
+  .then((data) => {
+    showProduct(data)
+  })
